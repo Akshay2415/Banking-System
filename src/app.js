61e804c580d29a159efec071a58a736ca1e038d7
Akshay2415 <akshaygirnare1234@gmail.com>
2026-04-12 15:42:24 +0530

    Creted a Schema for user accounts

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 const express = require("express")
-const authRouter = require("./routes/auth.route")
+const cookieParser = require("cookie-parser") 
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
+/**
+ * Controller required
+ */
+const authRouter = require("./routes/auth.route")
+const accountRouter = require("./routes/accounts.route")
+
+
+/**
+ * Controller use
+ */
+app.use("/api/account",accountRouter)
 app.use("/api/auth",authRouter)
 
 module.exports = app
---
+++ src/models/account.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+
+const accountSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required:[true,"Account must be associated with user"],
+        index:true
+        //index is use in mongo for optimizing 
+        //there are many users so to help for searching or sorting index is use , and follows the concept of B+ tree
+    },
+    status:{
+        enum:{
+            values:["ACTIVE","FROZEN","CLOSED"],
+            message:"Status can be either ACTIVE,FROZEN or CLOSED"
+        }
+    },
+    currency:{
+        type:String,
+        required:[true,"Currency is required"],
+        default:"INR"
+    }
+},{
+    timestamps:true
+})
+
+accountSchema.index({user:1 ,status:1});
+//compund index or compund schema is use to so we search user by index or on status also
+
+const accountModel = mongoose.model("account",accountSchema)
+
+module.exports = accountModel;
