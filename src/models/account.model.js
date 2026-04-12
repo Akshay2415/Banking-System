a34c434d677ac3ea806ff258081ffd1234608a1b
Akshay2415 <akshaygirnare1234@gmail.com>
2026-04-12 16:45:37 +0530

    Create middleware and routes for account

--- src/models/account.model.js
+++ src/models/account.model.js
@@ -10,10 +10,12 @@ const accountSchema = new mongoose.Schema({
         //there are many users so to help for searching or sorting index is use , and follows the concept of B+ tree
     },
     status:{
+        type:String,
         enum:{
             values:["ACTIVE","FROZEN","CLOSED"],
             message:"Status can be either ACTIVE,FROZEN or CLOSED"
-        }
+        },
+        default:"ACTIVE"
     },
     currency:{
         type:String,
--- src/routes/accounts.route.js
+++ src/routes/accounts.route.js
@@ -1,7 +1,16 @@
 const express = require("express")
+const authMiddleware = require("../middlewares/auth.middleware")
+const accountController = require("../controllers/accounts.controller")
 
 
 const router = express.Router()
 
+/**
+ * POST request
+ * /api/accounts/
+ * creating a new account
+ * Protected Route
+ */
+router.post("/",authMiddleware.authMiddleware,accountController.createAccountController)
 
 module.exports = router;
